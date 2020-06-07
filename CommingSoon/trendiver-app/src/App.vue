2abb82a9fcb558b966bd4f87919ee3d10ab98004
Esraa Abdel-Raheem <abdelraheemesraa@gmail.com>
2020-06-07 21:49:15 +0200

    make api works

--- CommingSoon/trendiver-app/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="app">
-    <div id="nav">
-       <!-- <router-link to="/">Dashboard</router-link>  -->
-       <traction></traction>
-    </div>
-    <router-view/>
-  </div>
-</template>

c0adb3d3cad8fe78b68441d0157ac47ffb780067
Esraa Abdel-Raheem <abdelraheemesraa@gmail.com>
2020-07-06 20:07:55 +0200

    adding svgs

---
+++ CommingSoon/App/components/userFlow/shared/footer.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="footer-section">
+    <div class="footer p-5 d-flex flex-row justify-content-around">
+      <div class="col-3">
+        <section class="d-flex flex-row">
+          <img src="../../../../build/logo.png" />
+          <p>
+            <b>Trendiver</b>
+          </p>
+        </section>
+        <p class="p-2">
+          Video on demand & paid
+          live streaming marketplace
+        </p>
+      </div>
+      <section class="inner-section col-3 d-flex flex-column">
+        <p>
+          <b>Site Map</b>
+        </p>
+        <a>Entertainment</a>
+        <a>Science & Knowledge</a>
+        <a>Sports</a>
+        <a>Arts & Literature</a>
+      </section>
+      <section class="col-3">
+        <p>
+          <b>Newsletter</b>
+        </p>
+        <p>
+          Sign up in our Newsletter
+          to get weekly updates
+        </p>
+        <form>
+          <input class="footer-input p-2 mb-2" placeholder="Enter your Email" />
+          <button class="btn footer-signup">Sign Up</button>
+        </form>
+      </section>
+    </div>
+    <div class="copy-right text-center">
+      <p class="p-3 m-0">Copyright &copy; 2016-2020 Trendiver.Com</p>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.footer-section {
+    margin-top: 20px;
+  .footer {
+    background: #e7ebee;
+    .inner-section {
+      a {
+        margin-bottom: 17px;
+      }
+    }
+    .footer-input {
+      background: #d3d9de;
+      border-radius: 8px;
+      border: none;
+      height: 39px;
+      width: 290px;
+      outline: none;
+    }
+    .footer-signup {
+      background: #feca55;
+      border-radius: 8px;
+      outline: none;
+      color: white;
+    }
+  }
+  .copy-right {
+    background: #d3d9de;
+  }
+}
+</style>
---
+++ CommingSoon/App/components/userFlow/sliderCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="d-flex flex-row justify-content-around">
+    <div class="card d-flex flex-column col-3">
+      <img svg-inline class="card-icon" src="../../assets/clock.svg" />
+      <p class="card-title">Unlimited Access</p>
+      <p>
+        self-based learning - whenever and wherever
+        you want. Graduate while learning part-time
+        for 10hrs/week.
+      </p>
+    </div>
+    <div class="card d-flex flex-column col-3">
+      <img svg-inline class="card-icon" src="../../assets/graduation.svg" />
+      <p class="card-title">Interaction learing </p>
+      <p>
+        self-based learning - whenever and wherever
+        you want. Graduate while learning part-time
+        for 10hrs/week.
+      </p>
+    </div>
+    <div class="card d-flex flex-column col-3">
+      <img svg-inline class="card-icon" src="../../assets/money.svg" />
+      <p class="card-title">Save Money</p>
+      <p>
+        self-based learning - whenever and wherever
+        you want. Graduate while learning part-time
+        for 10hrs/week.
+      </p>
+    </div>
+  </div>
+</template>
+
+
+<style lang="less" scoped>
+.card {
+  box-shadow: 0px 0px 5px rgba(19, 19, 51, 0.1);
+  text-align: center;
+  border-radius: 8px;
+  .card-icon {
+    align-self: center;
+    margin: 20px;
+  }
+  .card-title {
+    font-size: 22px;
+    color: #3512b1;
+  }
+}
+</style>

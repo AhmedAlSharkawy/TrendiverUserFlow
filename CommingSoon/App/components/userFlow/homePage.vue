b5a2289a95b82e5c4966920b689b4fababcbd05b
Esraa Abdel-Raheem <abdelraheemesraa@gmail.com>
2020-07-20 15:35:09 +0200

    working on main card component for videos

---
+++ CommingSoon/App/components/userFlow/homePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <uf-navbar></uf-navbar>
+    <sub-navbar></sub-navbar>
+    <slider-card></slider-card>
+    <br/>
+    <hr/>
+    <video-card></video-card>
+    <uf-footer></uf-footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ CommingSoon/App/components/userFlow/videoCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="card">
+      <img class="card-img-top" src alt="Card image cap" />
+      <div class="card-body">
+        <p class="sub-title">Recorded</p>
+        <p class="card-title">Card title</p>
+        <div class="card-content d-flex flex-row">
+          <img src="#" />
+          <p>Moo Islam</p>
+        </div>
+       
+        <p>EGP 1090</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+
+<style lang="less" scoped>
+.card {
+  box-shadow: 0px 0px 8px rgba(19, 19, 51, 0.15);
+  border-radius: 8px;
+  width: 17rem;
+  .card-body {
+    .sub-title {
+      font-size: 14px;
+    }
+    .card-title {
+      font-size: 18px;
+    }
+  }
+}
+</style>

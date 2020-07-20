66ca2f5224aa51b7e5ede288506e93a429711361
Esraa Abdel-Raheem <abdelraheemesraa@gmail.com>
2020-07-20 20:01:36 +0200

    adjust small detailed issues to the video card

--- CommingSoon/App/components/userFlow/videoCard.vue
+++ CommingSoon/App/components/userFlow/videoCard.vue
@@ -4,19 +4,16 @@
       <img class="card-img-top" src alt="Card image cap" />
       <div class="card-body">
         <p class="sub-title">Recorded</p>
-        <p class="card-title">Card title</p>
+        <p class="card-title">Business Analytics</p>
         <div class="card-content d-flex flex-row">
           <img src="#" />
-          <p>Moo Islam</p>
+          <p class="card-content-text">Moo Islam</p>
         </div>
-        <p>EGP 1090</p>
+        <p class="card-price">EGP 1090</p>
       </div>
     </div>
   </div>
 </template>
-
-
-
 
 <style lang="less" scoped>
 .card {
@@ -26,8 +23,13 @@
   .card-body {
     .sub-title {
       font-size: 14px;
+      color: #131333;
     }
     .card-title {
+      font-size: 18px;
+      color: #3512B1;
+    }
+    .card-price {
       font-size: 18px;
     }
   }

509c1459a6e6655985d5c07ed39ec8fa6f31c6e9
Esraa Abdel-Raheem <abdelraheemesraa@gmail.com>
2020-07-22 18:06:12 +0200

    adjust main card component

--- CommingSoon/App/components/userFlow/videoCard.vue
+++ CommingSoon/App/components/userFlow/videoCard.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="m-5">
+  <div>
     <div class="card">
       <div>
-      <img svg-inline class="card-img-top"  src="../../assets/work.svg" alt="Card image cap" />
+      <img svg-inline class="card-img-top" src="../../assets/work.svg" />
       </div>
       <div class="card-body">
-        <p class="sub-title m-0">Recorded</p>
+        <p class="sub-title mb-1">Recorded</p>
         <p class="card-title"> <b>Business Analytics</b></p>
         <div class="card-content d-flex flex-row">
           <img svg-inline class="mr-2"  src="../../assets/userPhoto.svg" />
           <p class="card-content-text">Moo Islam</p>
         </div>
-        <p class="card-price">EGP 1090</p>
+        <p class="card-price mt-3">EGP 1090</p>
       </div>
     </div>
   </div>

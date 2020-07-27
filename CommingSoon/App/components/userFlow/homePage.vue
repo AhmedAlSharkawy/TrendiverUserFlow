88475a171c55e89fabd15f269f70bb6d7c8db612
Esraa Abdel-Raheem <abdelraheemesraa@gmail.com>
2020-07-27 20:41:33 +0200

    make the instructor card but without image

--- CommingSoon/App/components/userFlow/homePage.vue
+++ CommingSoon/App/components/userFlow/homePage.vue
@@ -2,10 +2,14 @@
   <div>
     <uf-navbar></uf-navbar>
     <sub-navbar></sub-navbar>
-      <slider-card></slider-card>
-      <br />
-      <div class="container">
+    <slider-card></slider-card>
+    <br />
+    <div class="container">
       <video-card></video-card>
+
+      <div class="d-flex flex-row">
+        <instructor-card></instructor-card>
+      </div>
     </div>
     <pre-footer></pre-footer>
     <uf-footer></uf-footer>

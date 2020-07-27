786ce6768747ba04a2a02837846a0cfb192e5ab9
Esraa Abdel-Raheem <abdelraheemesraa@gmail.com>
2020-07-27 17:11:30 +0200

    adjusting footer alignment and trying to fix the pre-footer section in all screens

--- CommingSoon/App/components/userFlow/preFooter.vue
+++ CommingSoon/App/components/userFlow/preFooter.vue
@@ -3,19 +3,17 @@
     <img class="background-img" svg-inline src="../../assets/Background.svg" />
     <div class="pre-footer container">
       <div class="pre-footer-content">
-        <h1 class="mb-4">
+        <p class="pre-footer-title mb-md-4 mb-0">
           <b>Record, Upload, Monetize</b>
-        </h1>
-        <p>
-          A paragraph is a series of related sentences developing a central idea, called the topic.
-          Try to think about
-          <br />paragraphs in terms of thematicunity: a paragraph is a sentence or a group of
-          sentences that supports.
         </p>
-        <button class="creator-btn mt-4" type="submit"><b>Become a Creator</b></button>
+        <p class="mb-0">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu facilisis lorem, a laoreet lorem.
+          <br/> Donec ac turpis semper, viverra lorem vel, sagittis dui.
+        </p>
+        <button class="creator-btn mt-md-4 mt-0" type="submit"><b>Become a Creator</b></button>
       </div>
     </div>
-    <img class="instructor-img" src="../../assets/instructor.png"/>
+    <!-- <img class="instructor-img" src="../../assets/instructor.png"/> -->
   </div>
 </template>
 
@@ -29,10 +27,20 @@
   }
   .pre-footer {
     .pre-footer-content {
-      top: 100px;
+      top: 90px;
       color: #f8f7fd;
       position: absolute;
       line-height: 2.5;
+        @media only screen and (max-width: 600px) {
+          line-height: 1.5;
+          top: 0;
+        }
+      .pre-footer-title {
+        font-size: 40px;
+         @media only screen and (max-width: 600px) {
+           font-size: 20px;
+         }
+      }
     }
     .creator-btn {
       background: #feca55;
--- CommingSoon/App/components/userFlow/shared/footer.vue
+++ CommingSoon/App/components/userFlow/shared/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="footer-section">
-    <div class="footer p-md-5 p-4 justify-content-around">
-      <section class="col-md-4">
+    <div class="footer container pt-md-5 p-4 justify-content-around">
+      <section class="col-md-4 p-0">
         <section class="d-flex flex-row">
           <img src="../../../../build/63e5830e485df268d38495c37166b21e.png" />
           <p>
@@ -13,7 +13,7 @@
           <br />live streaming marketplace
         </p>
       </section>
-      <section class="inner-section col-md-4">
+      <section class="inner-section col-md-4 p-0">
         <p>
           <b>Site Map</b>
         </p>
@@ -22,7 +22,7 @@
         <a>Sports</a>
         <a>Arts & Literature</a>
       </section>
-      <section class="col-md-4">
+      <section class="col-md-4 p-0">
         <p>
           <b>Newsletter</b>
         </p>
@@ -45,8 +45,8 @@
 <style lang="less" scoped>
 .footer-section {
   margin-top: 20px;
+  background: #e7ebee;
   .footer {
-    background: #e7ebee;
     display: flex;
     flex-direction: row;
     @media only screen and (max-width: 600px) {

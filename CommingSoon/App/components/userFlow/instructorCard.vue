e23e54ed253644b1ea169c345be1c8347bc37353
Esraa Abdel-Raheem <abdelraheemesraa@gmail.com>
2020-07-27 20:44:14 +0200

    adding the card component-code

---
+++ CommingSoon/App/components/userFlow/instructorCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="instructor-card col-md-4">
+      <!-- <img svg-inline src="../../assets/work.svg" /> -->
+    <p class="card-title">
+      <b>IIP Institute</b>
+    </p>
+    <p class="card-sub-title">
+      <b>Bussines, Design, Dvelopment</b>
+    </p>
+    <p class="card-content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc, neque dolor lorem maecenas scelerisque. Orci, praesent nisl,
+      aenean ultrices commodo fusce. Sit viverra mauris sodales amet, amet hac. In fermentum morbi.
+    </p>
+  </div>
+</template>
+
+
+<style lang="less" scoped>
+.instructor-card {
+  background-color: #f3f5f7;
+  box-shadow: 0px 0px 8px rgba(19, 19, 51, 0.15);
+  border-radius: 8px;
+  position: relative;
+  .card-title {
+    color: #3512b1;
+  }
+  .card-sub-title {
+    color: #21252a;
+  }
+  .card-content {
+    color: #495057;
+  }
+}
+</style>

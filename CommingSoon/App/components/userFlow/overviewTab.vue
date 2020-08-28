6100b09819c8386e6800fcbc5e1538c227708a9f
Esraa Abdel-Raheem <abdelraheemesraa@gmail.com>
2020-08-28 13:23:40 +0200

    building the overview tab

---
+++ CommingSoon/App/components/userFlow/overviewTab.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="overview-tab col-md-9">
+    <p class="overview-title"><b>Course overview</b></p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Non neque non nibh eget eu.Mauris eget sit libero scelerisque mauris cras orci.
+      Lacus ultrices vitae enim augue mauris auctor consectetur tincidunt
+    </p>
+    <p class="learn-title"> What you will learn</p>
+    <section>
+        <img svg-inline src="../../assets/right.svg" />
+    </section>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.overview-tab {
+    .overview-title, 
+    .learn-title {
+        font-size: 32px;
+        color: #21252A;
+    }
+}
+</style>
